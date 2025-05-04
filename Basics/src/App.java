ee248238e8be6b961a1944080b162423146e087d
Balaji Sundar <balajisundardev@gmail.com>
2025-05-04 20:39:23 +0530

    Files uploaded successfully

---
+++ Basics/src/App.java
@@ -0,0 +1,11 @@
+
+import ThingsToKnow.UserInput;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+        // System.out.println("Hello, World!");
+        UserInput obj = new UserInput();
+        obj.GetAndPrint();
+    }
+}
---
+++ Basics/src/ThingsToKnow/UserInput.java
@@ -0,0 +1,15 @@
+package ThingsToKnow;
+
+import java.util.Scanner;
+
+public class UserInput {
+
+    public void GetAndPrint() {
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
+            System.out.println("The number is : " + n);
+        } catch (Exception ex) {
+            System.err.println(ex.getMessage());
+        }
+    }
+}
